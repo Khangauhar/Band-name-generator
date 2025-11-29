da48e074a24f0ccff45eb59e09b2d0f8b4c458d2
Gauhar Khan <ogauharkhan@icloud.com>
2025-11-28 18:05:47 -0800

    Update bandname.py

--- band-name-generator/bandname.py
+++ band-name-generator/bandname.py
@@ -1,5 +1,5 @@
 
 print("Welcome to the Band Name generator")
-city=input("Please enter the name of your city"/n)
-pet=input("what is the name of your pet"/n)
+city=input("Please enter the name of your city/n")
+pet=input("what is the name of your pet/n")
 print("Your band name could be " + city + " " + pet)
